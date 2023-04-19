5438505dbb0a034c4d48c328d358fc6380bfa84f
jolanta-idad <c00265619@itcarlow.ie>
2023-04-19 23:48:52 +0100

    fyp

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/*=============== SHOW SCROLL UP ICON ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.hero__data`)
+sr.reveal(`.hero__img`, { delay: 500 })
+sr.reveal(`.about__img`, { origin: 'left' })
+sr.reveal(`.about__content`, { origin: 'right' })
+sr.reveal(`.website__img`, { origin: 'left' })
+sr.reveal(`.blog__card, .service__card, .faqs__group, .footer`, { interval: 80 })
